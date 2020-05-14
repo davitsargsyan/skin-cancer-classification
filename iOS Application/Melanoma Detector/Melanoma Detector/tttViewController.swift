a8eacd387c0860184999ad02471ee024d4d3e110
davitsargsyan <dave.sargsyan14@gmail.com>
2020-05-14 14:39:37 +0400

    Add files via upload

---
+++ iOS Application/Melanoma Detector/Melanoma Detector/tttViewController.swift
@@ -0,0 +1,30 @@
+//
+//  tttViewController.swift
+//  Melanoma Detector
+//
+//  Created by David Sargsyan on 3/14/20.
+//  Copyright © 2020 David Sargsyan. All rights reserved.
+//
+
+import UIKit
+
+class tttViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
